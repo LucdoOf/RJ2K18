3df84ec69b39bc74209f2e523529d5354a856e32
ùposte <lucasg.18@hotmail.fr>
2018-08-17 21:51:39 +0200

    Première fenetre

--- src/fr/ragejam/Component.java
+++ src/fr/ragejam/Component.java
@@ -1,5 +1,143 @@
 package fr.ragejam;
 
+import static org.lwjgl.opengl.GL11.GL_BLEND;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
+import static org.lwjgl.opengl.GL11.GL_ONE_MINUS_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_SRC_ALPHA;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.glBlendFunc;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glViewport;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.util.glu.GLU;
+
+
 public class Component {
+	
+	public boolean running = false;
+	public static String title = "Jeu";
+	public static int scale = 4;
+	public static int width = 400 / scale * 2;
+	public static int height =  210 / scale * 2;
+	public static boolean tick = false;
+	public static boolean render = false;
+	
+	DisplayMode mode = new DisplayMode(width * scale, height * scale);
+	
+	public Component(){
+		initDisplay();
+	}
+	
+	public static void main(String[] args){
+		Component main = new Component();
+		main.start();
+	}
+	
+	public void update(){
+	}
+	
+	public void render(){
+		width = Display.getWidth() / scale;
+		height = Display.getHeight() / scale;
+		view2D(width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+	}
+	
+	public void start(){
+		running = true;
+		loop();
+	}
+
+	public void stop(){
+		running = false;
+	}
+	
+	public void exit(){
+		Display.destroy();
+		System.exit(0);
+	}
+	
+	public void initDisplay(){
+		try {
+			Display.setDisplayMode(mode);
+			Display.setResizable(false);
+			Display.setFullscreen(false);
+			Display.setTitle(title);
+			Display.create();
+			view2D(width, height);
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void view2D(int width, int height){
+		glViewport(0, 0, width * scale, height * scale);
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		GLU.gluOrtho2D(0, width, height, 0);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		
+		glEnable(GL_TEXTURE_2D);
+		
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+	
+	public void loop(){
+		
+		long timer = System.currentTimeMillis();
+		
+		long before = System.nanoTime();
+		double elapsed = 0;
+		double nanoSeconds = 1000000000.0 / 60.0;
+		
+		int ticks = 0;
+		int frames = 0;
+		
+		while(running){
+			if(Display.isCloseRequested()) stop();
+			Display.update();
+			
+			tick = false;
+			render = false;
+			
+			long now = System.nanoTime();
+			elapsed = now - before;
+
+			if(elapsed > nanoSeconds){
+				before += nanoSeconds;
+				tick = true;
+				ticks++;
+			} else {
+				render = true;
+				frames++;
+			}
+			
+			if(tick) update();
+			if(render) render();
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer+= 1000;
+				Display.setTitle("ticks: " + ticks + " ,fps: " + frames);
+				ticks = 0;
+				frames = 0;
+			}
+			
+		}
+		exit();
+	}
+
+	
+	
+	
 
 }
