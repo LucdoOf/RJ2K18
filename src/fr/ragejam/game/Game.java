3e8c4465ca43d1e573efbca8366fd0448b90f8c0
ùposte <lucasg.18@hotmail.fr>
2018-08-18 14:36:39 +0200

    Quand la musique est bonne..

--- src/fr/ragejam/game/Game.java
+++ src/fr/ragejam/game/Game.java
@@ -1,7 +1,13 @@
 package fr.ragejam.game;
 
+import java.io.IOException;
+
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+import org.newdawn.slick.openal.Audio;
+import org.newdawn.slick.openal.AudioLoader;
+import org.newdawn.slick.openal.SoundStore;
+import org.newdawn.slick.util.ResourceLoader;
 
 import fr.ragejam.Component;
 import fr.ragejam.game.entities.Player;
@@ -12,39 +18,49 @@ import fr.ragejam.graphics.Renderer;
 import fr.ragejam.graphics.Texture;
 
 public class Game {
-	
+
 	private Level level;
-	
+
 	public Game(String levelname){
 		this.level = new Level(levelname);
 		level.loadLevel();
 		level.addEntity(new Player(level, 20, 30));
+
+		try {
+			Audio oggEffect = AudioLoader.getAudio("OGG", ResourceLoader.getResourceAsStream("res/Delight.ogg"));
+			oggEffect.playAsMusic(1, 1, true);
+			SoundStore.get().poll(0);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+
 	}
-	
+
 	public void render(){
 		level.render();
 		Texture.gradiant.bind();
 		Renderer.quadData(0, 0, Component.width, Component.height);
 		Texture.gradiant.unbind();
 	}
-	
+
 	public void update(){
-		
+
 		if(Keyboard.isKeyDown(Keyboard.KEY_R)){
 			Component.setGame(new Game("level_test2"));
 		}
-		
+
 		level.update();
-		
+
 		if(level == null) return;
 		if(Mouse.isButtonDown(0)){
 			level.setTileAt((int)Component.getMouseX()/Tile.SIZE, (int)Component.getMouseY()/Tile.SIZE, new ModulableTile(0, level, (int)Component.getMouseX()/Tile.SIZE, (int)Component.getMouseY()/Tile.SIZE, Texture.tile_modulable_A));
 		} else if(Mouse.isButtonDown(1)){
 			level.setTileAt((int)Component.getMouseX()/Tile.SIZE, (int)Component.getMouseY()/Tile.SIZE, null);
 		}
-		
+
 	}
-	
+
 	public void translateView(float xa, float ya){
 		if(xa > 0) xa = 0;
 		if(ya > 0) ya = 0;
