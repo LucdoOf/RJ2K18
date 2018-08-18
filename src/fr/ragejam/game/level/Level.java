d8d4c31da8026f1e1070f571f7de47c28dd2cd29
ùposte <lucasg.18@hotmail.fr>
2018-08-18 22:41:49 +0200

    Oups

--- src/fr/ragejam/game/level/Level.java
+++ src/fr/ragejam/game/level/Level.java
@@ -33,7 +33,7 @@ public class Level {
 
 	public void render(){
 		Texture.background_g.bind();
-		Renderer.quadData(0, 0, Component.width, Component.width);
+		Renderer.quadData(0, 0, Component.width -Component.getXScroll(), Component.height-Component.getYScroll());
 		Texture.background_g.unbind();
 
 		List<Tile> toRender = new ArrayList<>(tiles);
